c17120c8155f8934abbe0fca015ee6d817c31790
LakshmiPriyanka1928 <dudyalapriyanka50@gmail.com.com>
2025-07-07 10:31:08 +0530

    Java_Fundamentals

---
+++ Question1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the string");
+		String a=sc.nextLine();
+		String b=sc.nextLine();
+		System.out.println(a+" Technologies "+b);
+	}
+
+}
