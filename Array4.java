4074ca5e8e04fc1bc8e6eb35738e47a1955c1d76
LakshmiPriyanka1928 <dudyalapriyanka50@gmail.com.com>
2025-07-07 15:53:54 +0530

    Java_Fundamentals

---
+++ Array4.java
@@ -0,0 +1,8 @@
+public class Array4 {
+    public static void main(String[] args) {
+        int[] asciiValues = {72, 101, 108, 108, 111, 32, 74, 97, 118, 97}; 
+        for (int value : asciiValues) {
+            System.out.print((char) value);
+        }
+    }
+}
